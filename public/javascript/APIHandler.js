3da7c35fdba6b462e7d7f14efca056b31aff18ca
Austin Mazur <mazuraustin1@gmail.com>
2023-04-25 22:03:47 +0200

    Make API handler class

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,32 @@
+const axios = require("axios");
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.api = axios.create({
+      baseURL: this.BASE_URL,
+    });
   }
 
-  getFullList () {
+  getFullList = () => {
+    return this.api.get("/characters");
+  };
 
+   getOneRegister = (characterId) => {
+    return this.api.get(`/characters/${characterId}`);
+  };
+
+  createOneRegister(characterInfo) {
+    return this.api.post("/characters", characterInfo);
   }
 
-  getOneRegister () {
-
+  updateOneRegister(characterId, characterInfo) {
+    return this.api.put(`/characters/${characterId}`, characterInfo);
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(characterId) {
+    return this.api.delete(`characters/${characterId}`);
   }
 }
+
+module.exports = APIHandler;
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,7 +2,14 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
+    .then((data) => {
+      console.log('data from click', data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    console.log("hi");
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
